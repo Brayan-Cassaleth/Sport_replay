50fc90a5a400f6a4997fe88c9e5d44202abf4fd4
Brayan-Cassaleth <b.cassalethr@gmail.com>
2026-08-01 14:24:59 -0500

    Creamos la ventana de la politica de privacidad de la pagina

--- js/login.js
+++ js/login.js
@@ -4,6 +4,7 @@ function datos(){
 
     if(usuario == "admin"){
         if(clave == "admin"){
+            alert("Esta cancha cuenta con un sistema de videograbación administrado por Sport Replay. Al ingresar y participar en los partidos, los usuarios aceptan que las grabaciones podrán utilizarse para permitir la visualización, descarga y análisis de sus encuentros, conforme a esta Política de Privacidad y a los Términos y Condiciones del servicio.")
             window.location.href = "carga_videos.html"
         }else{
             alert("Clave Incrrecta")
